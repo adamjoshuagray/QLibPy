25002f51a6cb35b2bdf11e4cc01a7bb8450cab3e
Adam Gray <adam.joshua.gray@gmail.com>
2015-07-12 14:43:27 +0800

    Bugfixes for function naming and brentq usage.

--- OptionRoutines.py
+++ OptionRoutines.py
@@ -1,16 +1,16 @@
 
 from scipy.stats import norm
 from scipy.optimize import brentq
-from maths import sqrt, log, exp
+from math import sqrt, log, exp
 
 # TODO: Add comments
 
 def _calc_d1(s, tau, k, r, sigma):
-    d1  = 1 / (sigma * sqrt(tau)) * (log(S / K) + (r + pow(sigma, 2) / 2) * (tau))
+    d1  = 1 / (sigma * sqrt(tau)) * (log(s / k) + (r + pow(sigma, 2) / 2) * (tau))
     return d1
 
 def _calc_d2(s, tau, k, r, sigma):
-    d2  = calc_d1(s, tau, k, r, sigma) - sigma * sqrt(t)
+    d2  = _calc_d1(s, tau, k, r, sigma) - sigma * sqrt(tau)
     return d2
 
 def calc_bs_call_px(s, tau, k, r, sigma):
@@ -26,7 +26,7 @@ def calc_bs_put_px(s, tau, k, r, sigma):
 def calc_bs_call_sigma(c, s, tau, k, r):
     fn      = lambda x: calc_bs_call_px(s, tau, k, r, x) - c
     # Include all realistic levels for sigma
-    a       = 0.0
+    a       = 0.0001
     b       = s * 10
     xtol    = 0.00000001 * c
     sigma   = brentq(fn, a, b, xtol = xtol)
@@ -35,7 +35,7 @@ def calc_bs_call_sigma(c, s, tau, k, r):
 def calc_bs_put_sigma(p, s, tau, k, r):
     fn      = lambda x: calc_bs_put_px(s, tau, k, r, x) - p
     # Include all realistic levels for
-    a       = 0.0
+    a       = 0.0001
     b       = s * 10
     xtol    = 0.00000001 * c
     sigma   = brentq(fn, a, b, xtol = xtol)
@@ -44,7 +44,7 @@ def calc_bs_put_sigma(p, s, tau, k, r):
 def calc_bs_call_r(c, s, tau, k, sigma, px):
     fn      = lambda x: calc_bs_call_px(s, tau, k, x, )
     # We realisitically expect interest rates to be between -100% and 100%
-    a       = -1.0
+    a       = -0.99
     b       = 1.0
     xtol    = 0.00000001 * c
     sigma   = brentq(fn, a, b, xtol = xtol)
@@ -53,7 +53,7 @@ def calc_bs_call_r(c, s, tau, k, sigma, px):
 def calc_bs_put_r(p, s, tau, k, sigma, px):
     fn      = lambda x: calc_bs_put_px(s, tau, k, x, )
     # We realisitically expect interest rates to be between -100% and 100%
-    a       = -1.0
+    a       = -0.99
     b       = 1.0
     xtol    = 0.00000001 * c
     sigma   = brentq(fn, a, b, xtol = xtol)
